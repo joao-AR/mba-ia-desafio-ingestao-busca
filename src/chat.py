bd3f0b2b05eab7a9e1bb49fc167f932f8bd0e7ed
João Pedro Rodrigues <jp.alvesrodrigues02@gmail.com>
2026-04-17 08:47:29 -0300

    feat(chat): implement continous chat loop with exit command

--- src/chat.py
+++ src/chat.py
@@ -3,7 +3,6 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 from langchain_postgres import PGVector
 from search import search_prompt
-import sys
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -18,24 +17,44 @@ store = PGVector(embeddings=embeddings,
                 connection=os.getenv("DATABASE_URL"),
                 use_jsonb=True)
 
+def getUserQuestion():
+    return input("Qual sua pergunta? (escreva exit para sair)\n").strip()
 
+def queryValidator(query):
+    if not query:
+        print("Pergunta não pode ser vazia")
+        return False
+    elif query.lower() == "exit":
+        print("Saindo...")
+        return False
+ 
+    return True
+
+def getQueryResults(query):
+    documents = store.similarity_search_with_score(query, k=10)
+    results = "\n".join([document.page_content for document, score in documents])  
+    return results
+ 
 def main():
-    
-    query = input("Whats is your question?")
-
-    # Verifica se não está vazia
-    if query.strip():
-        results = store.similarity_search_with_score(query, k=4)
-    
     chain = search_prompt()
 
     if not chain:
         print("Não foi possível iniciar o chat. Verifique os erros de inicialização.")
         return
     
-    result = chain.invoke({"contexto": results, "pergunta": query})
+    while True:
+        query = getUserQuestion()
+        isValidQuery = queryValidator(query)
+        
+        if not isValidQuery:
+            return
     
-    print(result.content)
+        results = getQueryResults(query)
+        
+        
+        
+        result = chain.invoke({"contexto": results, "pergunta": query})
+        print(result.content)
 
 if __name__ == "__main__":
     main()
